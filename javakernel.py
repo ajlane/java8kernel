50779023251c14ae204d5f4823e076ac21e0e3f6
Aaron Lane <a.j.lane@gmail.com>
2016-03-27 06:43:11 +0000

    Added a little parsing to make multi-line expressions work and remove the command echo.

--- javakernel.py
+++ javakernel.py
@@ -1,4 +1,5 @@
 from IPython.kernel.zmq.kernelbase import Kernel
+import re
 import pexpect
 
 
@@ -14,10 +15,11 @@ class JavaKernel(Kernel):
     banner=repl.before.decode('utf-8')
 
     def do_execute(self, code, silent, store_history=True, user_expressions=None, allow_stdin=False):
-        self.repl.sendline(code)
+        self.repl.sendline(re.sub(r'\s+', ' ', code, flags=re.MULTILINE))
         self.repl.expect('java>')
+        response=self.repl.before.decode('utf-8')
         if not silent:
-            stream_content = {'name':'stdout', 'text': self.repl.before.decode('utf-8')}
+            stream_content = {'name':'stdout', 'text': response.split('\n',1)[1]}
             self.send_response(self.iopub_socket, 'stream', stream_content)
             return {
                 'status': 'ok',
